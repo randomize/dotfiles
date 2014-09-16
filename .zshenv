18238f56027070e0dd687e49fb817d2a0e007f02
Randy <randomize46@gmail.com>
2014-09-16 09:28:51 +0300

    Setting up i3, got nice fonts, zshenv with exports etc.

---
+++ .zshenv
@@ -0,0 +1,16 @@
+# Setup defaukts
+export BROWSER=firefox
+export EDITOR=vim
+
+# Enable anti aliasing in java programs
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
+
+# Set path for gtk config
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0" 
+# export PAGER="most -s"
+export LESS="-i -s -R"
+export LESSCHARSET='utf-8'
+export PAGER="less"
+export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
+
+export TERMINAL="urxvtc"
--- bin/toggle-scratchpad.sh
+++ bin/toggle-scratchpad.sh
@@ -6,13 +6,35 @@ classname=randy_scratchpad_$1
 
 wid=$( xdotool search --classname ${classname} )
 
+width=150
+height=40
+posx=370
+posy=440
+
+case "$1" in
+   ranger)
+      posx=136
+      posy=300
+      width=200
+      height=50
+      param="-depth 0"
+      ;;
+
+   ncmpcpp)
+      posx=140
+      posy=160
+      width=210
+      height=55
+      ;;
+esac
+
 if [ -z "$wid" ]; then
     #your prefered urxvt output here.
     #urxvt -pe default,matcher -name scratchpad_urxvt_top -geometry 180x28
-    urxvtc -name "${classname}" -geometry 272x50 -e $@
+    urxvtc ${param} -name "${classname}" -geometry "${width}x${height}" -e $@
     #xdotool search --classname ${classname} windowsize 1800 500
 
-    i3-msg "[instance=\"${classname}\"] floating enable; move position 0px 0px; move scratchpad"
+    i3-msg "[instance=\"${classname}\"] floating enable; move position ${posx}px ${posy}px; border pixel 1; move scratchpad"
 
     # i3-msg "[instance=\"${classname}\"] move scratchpad"
     #for_window [instance="metask"] floating enable;
