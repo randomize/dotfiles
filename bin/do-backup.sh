6107eb23a7087055c4f0c40da57d68dd67abd7b8
Randy <randomize46@gmail.com>
2016-02-20 16:57:33 +0200

    add autorun for ddc-bright, wip on backup script, some ignores

--- bin/do-backup.sh
+++ bin/do-backup.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-DOT_DST_DIR=/home/randy/nfo/global_dotfiles
+BACKUP_DIR=/mnt/data/backups
+
+
+# Current backup to be created
+BACKUP_TMP="${BACKUP_DIR}/cur"
+mkdir "${BACKUP_TMP}"
+
+DOT_DST_DIR="${BACKUP_TMP}/global_dotfiles"
+PRIVATE_DST_DIR="${BACKUP_TMP}/private"
 
 # Copy standard dotfiles
-cp /etc/fstab $DOT_DST_DIR/etc/fstab
-cp /etc/modules-load.d/* $DOT_DST_DIR/etc/modules-load.d/
+cp /etc/fstab "${DOT_DST_DIR}/etc/fstab"
+cp /etc/modules-load.d/* "${DOT_DST_DIR}/etc/modules-load.d/"
+
+# TODO: Copy
+# Private and secure
+#/.ssh/
+#/.2steps
+#/.itmages.conf
+#/client.ovpn
+#mutt
+#profanity?
+
+# TODO: Archive and delete temp files #
 
 
 
