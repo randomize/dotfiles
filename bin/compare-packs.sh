455004b492629fa9d2f1cb3b62dbd9382e7d6c5e
Eugene Mihailenco <emihailenco@protonmail.com>
2020-01-27 12:07:54 +0200

    Cleanup: Some random updates and changes to dotfiles

--- bin/compare-packs.sh
+++ bin/compare-packs.sh
@@ -4,7 +4,7 @@ rm -f /tmp/mentioned.txt
 rm -f /tmp/locals.txt
 
 cat ~/nfo/paks.txt | sed "s/\/\/.*//;/^\s*$/d;/:/d;/=/d;s/\s*//;s/.*\///;s/ *//g" | sort > /tmp/mentioned.txt
-yaourt -Qe | sed -e "s/.*\///g" | sed -e "s/ .*//" | sort > /tmp/locals.txt
+yay -Qe | sed -e "s/.*\///g" | sed -e "s/ .*//" | sort > /tmp/locals.txt
 
 sleep 1
 
