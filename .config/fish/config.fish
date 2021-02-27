df2f4511248b270050a5b7e0c0566dd9ad816148
Eugene Mihailenco <emihailenco@protonmail.com>
2021-02-27 21:02:52 +0200

    Fish: Better way to handle fisher - own dir to not clutter my config dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,15 @@
 set -gx EDITOR nvim
 
 set -gx fisher_path ~/.config/fish/fisher_plugins
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    source $file
+end
 
 
 . "$HOME/.config/fish/aliases.fish"
-
 
 # My custom binding to ctrl+t to search including ignored files
 function __fzf_search_current_dir_everything
