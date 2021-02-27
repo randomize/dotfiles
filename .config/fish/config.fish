4d90e02269cc994359e0ba50977d3671eb9195b2
Eugene Mihailenco <emihailenco@protonmail.com>
2021-02-27 19:07:25 +0200

    Fish: Url enc/dec helper, more hotkeys te mimick fzf behaviour in zsh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,24 @@ direnv hook fish | source
 starship init fish | source
 
 . "$HOME/.config/fish/aliases.fish"
+
+
+# My custom binding to ctrl+t to search including ignored files
+function __fzf_search_current_dir_everything
+    set -lx fzf_fd_opts -H -I
+    __fzf_search_current_dir
+end
+function __fzf_search_current_dir_dirs
+    set -lx fzf_fd_opts -H -I --type=d
+    __fzf_search_current_dir
+end
+
+bind --mode insert \ct __fzf_search_current_dir_everything
+bind --mode insert \ec __fzf_search_current_dir_dirs
+
+function urlencode
+  perl -MURI::Escape -le "print uri_escape('$argv')"
+end
+function urldecode
+  perl -MURI::Escape -le "print uri_unescape('$argv')"
+end
